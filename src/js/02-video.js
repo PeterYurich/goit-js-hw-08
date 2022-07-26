96208bf969a3a3c7cd3653a4138860e7558f99af
Peter <peteryurich@gmail.com>
2022-07-26 21:48:42 +0300

    ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,14 +5,12 @@ import _ from 'lodash';
 const iframe  = document.querySelector("#vimeo-player");
 const player = new Player(iframe);
 
-console.log(player)
 
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
 
 const onPlay = function(data) {
     const time = data.seconds
     localStorage.setItem("videoplayer-current-time", time);
-    console.log('asdf')
 };
 
 player.on('timeupdate', _.throttle(onPlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,47 @@
 
-console.log('dfg')
+import { _ } from "lodash"
+
+const feedback = {
+    email: '',
+    message: ''
+}
+
+// find objects:
+const emailInputEl = document.querySelector('[name="email"]');
+const messageInputEl = document.querySelector('[name="message"]');
+const submitBtnEl = document.querySelector('[type="submit"]');
+
+// set entered values:
+emailInputEl.value = JSON.parse(localStorage.getItem("feedback-form-state")).email
+messageInputEl.value = JSON.parse(localStorage.getItem("feedback-form-state")).message
+
+
+// update localStorage:
+const onEmailInput = function(e) {
+    e.preventDefault()
+    feedback.email = e.currentTarget.value
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedback))
+}
+emailInputEl.addEventListener('input', _.throttle(onEmailInput, 1))
+
+const onMessageInput = function(e) {
+    e.preventDefault()
+    feedback.message = e.currentTarget.value
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedback))
+}
+messageInputEl.addEventListener('input', _.throttle(onMessageInput, 1))
+
+
+// get values and clear the form:
+submitBtnEl.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")))
+    
+    feedback.email = ''
+    emailInputEl.value = ''
+    
+    feedback.message = ''
+    messageInputEl.value = ''
+    
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedback))
+})
