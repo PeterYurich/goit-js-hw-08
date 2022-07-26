5a17773491c4f1ddd5311676db31447bbcfdb99a
Peter <peteryurich@gmail.com>
2022-07-26 17:44:04 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,19 @@
 import Player from '@vimeo/player'
 
+import _ from 'lodash';
+
 const iframe  = document.querySelector("#vimeo-player");
 const player = new Player(iframe);
 
 console.log(player)
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+
+const onPlay = function(data) {
+    const time = data.seconds
+    localStorage.setItem("videoplayer-current-time", time);
+    console.log('asdf')
+};
+
+player.on('timeupdate', _.throttle(onPlay, 1000));
+
