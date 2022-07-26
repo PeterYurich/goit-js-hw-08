3ed691c2a45566defbfb612d1e695acd7a51edd8
PeterYurich <97597653+PeterYurich@users.noreply.github.com>
2022-07-26 11:48:40 +0000

    Deploying to gh-pages from @ PeterYurich/goit-js-hw-08@f1f1caf61a110b81f2f45b31bd193b4b04cb8c3d 🚀

---
+++ 02-video.947b99e1.js
@@ -0,0 +1,2 @@
+document.querySelector("#vimeo-player"),Vimeo.Player();
+//# sourceMappingURL=02-video.947b99e1.js.map
---
+++ 02-video.eed7c3de.js
@@ -0,0 +1,2 @@
+document.querySelector("#vimeo-player"),Vimeo.Player();
+//# sourceMappingURL=02-video.eed7c3de.js.map
